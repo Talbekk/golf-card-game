bf2a86ef21142fb8da10bd0ebbdd2c2f5e0f51ee
Talbekk <mt.campbell@outlook.com>
2020-02-12 12:46:26 +0000

    refactors intro screen game ruless info into a component

---
+++ client/src/components/GameRules.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div id="game-rules">
+    <h4>The Deal:</h4>
+    <p>
+      Each player is dealt 4 cards face down from the deck. The remainder of the cards are placed face down, and the top card is turned up to start the discard pile beside it. Players arrange their 4 cards in a row face down. At the start of a round, the player can look at two cards but only for a short time.
+    </p>
+    <h4>GamePlay:</h4>
+    <p>
+      The object is for players to have the lowest value of the cards in front of them by either swapping them for lesser value cards or by pairing them up with cards of equal rank. A game is nine "holes" (deals), and the player with the lowest total score is the winner.
+    </p>
+      Beginning with the player to the dealer's left, players take turns drawing single cards from either the stock or discard piles. The drawn card may either be swapped for one of that player's 4 cards, or discarded. If the card is swapped for one of the face down cards, the card swapped in remains face up. If the player choose not to take the card, they must turn over one of their own. The round ends when all of a player's cards are face-up.
+    </p>
+   <h4>Scoring:</h4>
+   <p>
+     Each ace counts 1 point.
+     Each 5 counts minus 5 points.
+     Every other numeral card from 2 to 10 scores face value.
+     Each jack or queen scores 10 points.
+     Each king scores zero points.
+     A pair of equal cards scores zero points for the column (even if the equal cards are 5s).
+   </p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "game-rules"
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
--- client/src/components/IntroScreen.vue
+++ client/src/components/IntroScreen.vue
@@ -1,25 +1,7 @@
 <template lang="html">
   <div class="">
   <div id="intro-screen" v-if="!selectScoresPage">
-    <h4>The Deal:</h4>
-    <p>
-      Each player is dealt 4 cards face down from the deck. The remainder of the cards are placed face down, and the top card is turned up to start the discard pile beside it. Players arrange their 4 cards in a row face down. At the start of a round, the player can look at two cards but only for a short time.
-    </p>
-    <h4>GamePlay:</h4>
-    <p>
-      The object is for players to have the lowest value of the cards in front of them by either swapping them for lesser value cards or by pairing them up with cards of equal rank. A game is nine "holes" (deals), and the player with the lowest total score is the winner.
-    </p>
-      Beginning with the player to the dealer's left, players take turns drawing single cards from either the stock or discard piles. The drawn card may either be swapped for one of that player's 4 cards, or discarded. If the card is swapped for one of the face down cards, the card swapped in remains face up. If the player choose not to take the card, they must turn over one of their own. The round ends when all of a player's cards are face-up.
-    </p>
-   <h4>Scoring:</h4>
-   <p>
-     Each ace counts 1 point.
-     Each 5 counts minus 5 points.
-     Every other numeral card from 2 to 10 scores face value.
-     Each jack or queen scores 10 points.
-     Each king scores zero points.
-     A pair of equal cards scores zero points for the column (even if the equal cards are 5s).
-   </p>
+    <game-rules></game-rules>
    <div id="name-box" v-if="!newGame">
      <h4>Please enter the name of your golfer:</h4>
      <form id="user-name" v-on:submit.prevent="handleClick">
@@ -27,7 +9,6 @@
        <input id="submit" type="submit" value="Tee Off">
       </form>
   </div>
-    <!-- <button v-if='!newGame' v-on:click='clickedNewGame'>New Game</button> -->
     <button v-if='newGame' v-on:click='clickedDealCards'>Tee Off</button>
   </div>
   <div id="score-container">
@@ -41,6 +22,7 @@
 <script>
 import {eventBus} from '../main.js';
 import ScoresPage from './ScoresPage.vue';
+import GameRules from './GameRules.vue';
 
 export default {
   name: 'intro-screen',
@@ -52,7 +34,8 @@ export default {
     }
   },
   components: {
-    "scores-page": ScoresPage
+    "scores-page": ScoresPage,
+    "game-rules": GameRules
   },
   methods: {
     clickedNewGame(){
