8fcb675892a935ee85c6ac4c6ba06db4d8e2a872
Talbekk <mt.campbell@outlook.com>
2020-05-12 21:26:12 +0100

    adds styling to back of profile info

--- client/src/components/home/PlayerProfile.vue
+++ client/src/components/home/PlayerProfile.vue
@@ -4,20 +4,21 @@
       <b-button id="btn-main">Edit Profile</b-button>
       <b-button id="btn-main" v-on:click="signOut">Sign Out</b-button>
     </div>
+    <div id="information-row">
     <div id="photo-box">
       <img src="../../assets/profile-default.jpg">
     </div>
     <div id="profile-info-box">
       <div id="profile-info-box-title">
-        <h5><b>Profile:</b></h5>
+        <h2><b>{{this.userData.username}}</b></h2>
       </div>
       <div id="profile-info-box-data">
-    <p><b>Golfer Name:</b> {{this.userData.username}}</p>
     <p><b>Games Played:</b> {{this.gamesPlayed}}</p>
     <p><b>Total Score:</b> {{this.totalScore}}</p>
     <p><b>Average Score:</b> {{this.averageScore}}</p>
       </div>
     </div>
+  </div>
   </div>
 </template>
 
@@ -58,7 +59,7 @@ export default {
   background-size: cover;
   color: #333;
   box-shadow: 0 30px 80px 10px rgb(0,0,0,0.8);
-  width: 100%;
+  width: 75%;
   font-size: 150%;
 }
 
@@ -70,17 +71,47 @@ export default {
   width: 100%;
 }
 
+#information-row {
+  display: flex;
+  flex-wrap: wrap;
+  /* flex-direction: row;
+  justify-content: flex-start; */
+  width: 100%;
+}
+
+#profile-info-box {
+  width: 100%;
+  padding: 0 20px;
+  flex: 4;
+}
+
+#profile-info-box-title {
+  border: 1px solid black;
+  border-radius: 5px;
+  padding: 5px;
+  color: #fff;
+  background-color: #000;
+}
+
+#profile-info-box-data {
+  border: 1px solid #000;
+  border-radius: 5px;
+  background-color: #555;
+  color: #fff;
+}
+
 #button-row #btn-main:last-child {
   margin-left: 25px;
 }
 
 #photo-box {
-  border: 5px solid black;
+  flex: 1;
 }
 
 #photo-box img {
   height: 300px;
   width: 200px;
+  border: 5px solid black;
 }
 
 
