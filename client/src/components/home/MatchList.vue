4fd3a338b288633e4b2463ceccbe7ae4ddb7c702
Talbekk <mt.campbell@outlook.com>
2020-05-24 16:50:03 +0100

    adds view scorecard button

--- client/src/components/home/MatchList.vue
+++ client/src/components/home/MatchList.vue
@@ -1,34 +1,45 @@
 <template lang="html">
-  <div id="match-list">
-    <div>
+  <div>
+    <div id="match-list">
       <b-table large hover head-variant="dark" border :items="matches" :fields="fields">
         <template v-slot:cell(averageScore)="data">
           {{ getAverageScore(data) }}
         </template>
+        <template v-slot:cell(card)="data">
+          <b-button v-on:click="showScoreCard(data)"id="btn-main">View</b-button>
+        </template>
       </b-table>
     </div>
+    <b-modal v-model="modalShow" scrollable title="Score Card" ok-only>
+      <score-card :scoreCard="score"/></score-card>
+    </b-modal>
   </div>
 </template>
 
 <script>
 import MatchDetails from './MatchDetails.vue';
+import ScoreCard from '../ScoreCard.vue';
 
 export default {
   name: 'match-list',
   props:['matches'],
   data() {
     return {
+      modalShow: false,
+      score: [],
       fields: [
         { key: 'date.date', label: 'Date', sortable: true },
         { key: 'card.length', label: 'Length', sortable: false },
-        { key: 'averageScore', label: 'Average', sortable: true },
-        { key: 'score', sortable: true }
+        { key: 'averageScore', label: 'Average' },
+        { key: 'score', sortable: true },
+        { key: 'card', label: 'Card'}
       ],
       averageScore: 0
     }
   },
   components: {
-    "match-details": MatchDetails
+    "match-details": MatchDetails,
+    "score-card": ScoreCard
   },
   methods: {
     getAverageScore(value){
@@ -36,6 +47,11 @@ export default {
       const totalScore = value.item.score;
       const average = totalScore / totalGames;
       return Math.floor(average);
+    },
+    showScoreCard(data){
+      this.modalShow = true;
+      this.score = data.item.card;
+      console.log("modal", this.modalShow);
     }
   }
 }
