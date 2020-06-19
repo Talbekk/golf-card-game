99a05dcfd59d65d278216ada013c36323a22f98e
Talbekk <mt.campbell@outlook.com>
2020-06-19 23:42:01 +0100

    shows computer cards

---
+++ client/src/components/computer/ComputerCard.vue
@@ -0,0 +1,90 @@
+<template lang="html">
+  <div id="player-card-container">
+    <li>
+    <img class="card-icon" v-if="hidden" v-on:click="handleClick" v-on:dblclick="lockCard" src="../../assets/CardBack.png"/>
+    <img class="card-icon" v-if="!hidden" :src="this.computerCard.image"/>
+  </li>
+</div>
+</template>
+
+<script>
+
+import {eventBus} from '../../main.js';
+
+export default {
+  name: 'computer-card',
+  props: ['computerCard', 'shownCards', 'lockedCards', 'counter', 'topCardSelected'],
+  data () {
+    return {
+      hidden: true,
+      lockedIn: false,
+      popoverShow: false,
+    }
+  },
+  watch: {
+    lockedCards(){
+      if (this.lockedCards.length === 0){
+        this.resetCard();
+      }
+    }
+  }
+//   methods: {
+//     handleClick(){
+//       eventBus.$emit("player-clicked-card", this.playerCard);
+//       if(this.shownCards < 2 && this.counter === 0){
+//       this.showCard();
+//     }
+//     if(this.shownCards === 2 && this.topCardSelected === true){
+//       this.switchCard();
+//     }
+//     },
+//     showCard() {
+//         this.hidden = false;
+//         setTimeout(()=> { this.hideCard() },2000);
+//         eventBus.$emit('shown-a-card', 1);
+//     },
+//     revealCard() {
+//       if(this.lockedIn === false){
+//         this.hidden = true;
+//       } else {
+//         this.hidden = false;
+//       }
+//     },
+//     hideCard(){
+//       if (this.lockedIn === false){
+//       this.hidden = true;
+//     }
+//     },
+//     lockCard() {
+//       eventBus.$emit('card-value', this.playerCard)
+//       this.lockedIn = true;
+//       this.revealCard();
+//     },
+//     switchCard() {
+//       eventBus.$emit('player-card', this.playerCard)
+//       this.lockedIn = true;
+//       this.revealCard();
+//     },
+//     resetCard(){
+//       this.hidden = true;
+//       this.lockedIn = false;
+//     }
+//   }
+}
+</script>
+
+<style lang="css" scoped>
+
+#player-card-container {
+  padding: 0.5em;
+}
+
+
+.card-icon {
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 10em;
+  max-height: 10em;
+}
+
+</style>
--- client/src/components/computer/ComputerCards.vue
+++ client/src/components/computer/ComputerCards.vue
@@ -1,12 +1,15 @@
 <template lang="html">
   <div id="cards-container">
     <p>Computer</p>
+     <ul>
+      <computer-card v-for="(computer, index) in computerCards" :computerCard='computerCard' :key='index' :index='index' :shownCards='shownCards' :lockedCards='lockedCards' :counter='counter' :topCardSelected='topCardSelected'></computer-card>
+    </ul>
   </div>
 </template>
 
 <script>
 import {eventBus} from '../../main.js';
-import PlayerCard from '../PlayerCard.vue';
+import ComputerCard from './ComputerCard.vue';
 
 export default {
   name: 'computers-cards',
@@ -15,9 +18,23 @@ export default {
       return {
           shownCards: 0
       }
+  },
+  components: {
+      "computer-card": ComputerCard
   }
 }
 </script>
 
 <style lang="css" scoped>
+
+ul {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  margin-left: auto;
+  margin-right: auto;
+  grid-row-gap: 2px;
+  padding-inline-start: 0px;
+  list-style-type: none;
+}
+
 </style>
